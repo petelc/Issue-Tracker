4ef379baa125e226cb020ff9007aff83f5af3b94
Pete Carroll <petelc1966@hotmail.com>
2020-08-08 04:34:12 -0400

    changed implementation to adjust for Material UI

--- ui/src/IssueFilter.jsx
+++ ui/src/IssueFilter.jsx
@@ -1,33 +1,92 @@
 /* eslint-disable linebreak-style */
+/* eslint-disable no-undef */
+/* eslint-disable react/prop-types */
 /* eslint-disable class-methods-use-this */
 /* eslint-disable linebreak-style */
 /* eslint "react/prefer-stateless-function": "off" */
 import React from 'react';
-import { Link } from 'react-router-dom';
-import Button from '@material-ui/core/Button';
+import { useHistory, useParams } from 'react-router-dom';
+import { makeStyles, withStyles } from '@material-ui/core/styles';
+import InputLabel from '@material-ui/core/InputLabel';
+import MenuItem from '@material-ui/core/MenuItem';
+import FormControl from '@material-ui/core/FormControl';
+import Select from '@material-ui/core/Select';
+import InputBase from '@material-ui/core/InputBase';
 
-export default class IssueFilter extends React.Component {
-  render() {
-    return (
+const BootstrapInput = withStyles((theme) => ({
+  root: {
+    'label + &': {
+      marginTop: theme.spacing(3),
+    },
+  },
+  input: {
+    borderRadius: 4,
+    position: 'relative',
+    backgroundColor: theme.palette.background.paper,
+    border: '1px solid #ced4da',
+    fontSize: 16,
+    padding: '10px 26px 10px 12px',
+    transition: theme.transitions.create(['border-color', 'box-shadow']),
+    // Use the system font instead of the default Roboto font.
+    fontFamily: [
+      '-apple-system',
+      'BlinkMacSystemFont',
+      '"Segoe UI"',
+      'Roboto',
+      '"Helvetica Neue"',
+      'Arial',
+      'sans-serif',
+      '"Apple Color Emoji"',
+      '"Segoe UI Emoji"',
+      '"Segoe UI Symbol"',
+    ].join(','),
+    '&:focus': {
+      borderRadius: 4,
+      borderColor: '#80bdff',
+      boxShadow: '0 0 0 0.2rem rgba(0,123,255,.25)',
+    },
+  },
+}))(InputBase);
+
+const useStyles = makeStyles((theme) => ({
+  margin: {
+    margin: theme.spacing(1),
+  },
+}));
+
+export default function IssueFilter() {
+  const classes = useStyles();
+
+  let { status } = useParams();
+  const history = useHistory();
+  const onChangeStatus = (e) => {
+    status = e.target.value;
+    history.push({
+      pathname: '/issues',
+      search: status ? `?status=${e.target.value}` : '',
+    });
+  };
+
+  return (
       <div className="masterContainer">
-          <Link to="/#/issues">
-            <Button variant="outlined">
-              All Issues
-            </Button>
-          </Link>
-          { ' | '}
-          <Link to={{ pathname: '/issues', search: '?status=New' }}>
-            <Button variant="outlined">
-              New Issues
-            </Button>
-          </Link>
-          { ' | '}
-          <Link to={{ pathname: '/issues', search: '?status=Assigned' }}>
-            <Button variant="outlined">
-              Assigned Issues
-            </Button>
-          </Link>
+          <FormControl className={classes.margin}>
+        <InputLabel id="demo-customized-select-label">Status</InputLabel>
+        <Select
+          labelId="demo-customized-select-label"
+          id="demo-customized-select"
+          value={status}
+          onChange={onChangeStatus}
+          input={<BootstrapInput />}
+        >
+          <MenuItem value="">
+            <em>(ALL)</em>
+          </MenuItem>
+          <MenuItem value={'New'}>New</MenuItem>
+          <MenuItem value={'Assigned'}>Assigned</MenuItem>
+          <MenuItem value={'Fixed'}>Fixed</MenuItem>
+          <MenuItem value={'Closed'}>Closed</MenuItem>
+        </Select>
+      </FormControl>
       </div>
-    );
-  }
+  );
 }
