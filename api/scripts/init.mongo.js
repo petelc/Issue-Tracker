cdcbbbe03486dcca7191fb2b092edcc2b7c1025e
Pete Carroll <petelc1966@hotmail.com>
2020-08-08 05:33:36 -0400

    adding new data-this seeds the database

--- api/scripts/init.mongo.js
+++ api/scripts/init.mongo.js
@@ -25,6 +25,14 @@ async function processInitialization() {
         created: new Date(2020, 8, 23),
         due: undefined,
         title: 'Error in console when clicking Add',
+        description: 'Steps to recreate the problem:'
+          + '\n1. Refresh the browser.'
+          + '\n2. Select "New" in the filter'
+          + '\n3. Refresh the browser again. Note the warning in the console:'
+          + '\n   Warning: Hash history cannot PUSH the same path; a new entry'
+          + '\n   will not be added to the history stack'
+          + '\n4. Click on Add.'
+          + '\n5. There is an error in console, and add doesn\'t work.',
       },
       {
         id: 2,
@@ -34,6 +42,8 @@ async function processInitialization() {
         created: new Date(2020, 5, 16),
         due: new Date(2020, 7, 16),
         title: 'Missing bottom border on panel',
+        description: 'There needs to be a border in the bottom in the panel'
+          + ' that appears when clicking on Add',
       },
     ];
     console.log(collection);
