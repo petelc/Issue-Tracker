a0186bebea92b4fcc669e6576ec65bba0917850c
Pete Carroll <petelc1966@hotmail.com>
2020-08-25 05:09:49 -0400

    ReDirect

---
+++ ui/dist/server.31b6a7c5a6f417d8485f.hot-update.js
@@ -0,0 +1,145 @@
+exports.id = "server";
+exports.modules = {
+
+/***/ "./server/render.jsx":
+/*!***************************!*\
+  !*** ./server/render.jsx ***!
+  \***************************/
+/*! exports provided: default */
+/***/ (function(module, exports) {
+
+throw new Error("Module build failed (from ./node_modules/babel-loader/lib/index.js):\nSyntaxError: C:\\Development\\MERN\\Issue-Tracker\\ui\\server\\render.jsx: Unexpected token (27:46)\n\n\u001b[0m \u001b[90m 25 | \u001b[39m  \u001b[36mconst\u001b[39m context \u001b[33m=\u001b[39m {}\u001b[33m;\u001b[39m\u001b[0m\n\u001b[0m \u001b[90m 26 | \u001b[39m  \u001b[36mconst\u001b[39m element \u001b[33m=\u001b[39m (\u001b[0m\n\u001b[0m\u001b[31m\u001b[1m>\u001b[22m\u001b[39m\u001b[90m 27 | \u001b[39m    \u001b[33m<\u001b[39m\u001b[33mStaticRouter\u001b[39m location\u001b[33m=\u001b[39m{req\u001b[33m.\u001b[39murl} context\u001b[33m=\u001b[39m{\u001b[33m>\u001b[39m\u001b[0m\n\u001b[0m \u001b[90m    | \u001b[39m                                              \u001b[31m\u001b[1m^\u001b[22m\u001b[39m\u001b[0m\n\u001b[0m \u001b[90m 28 | \u001b[39m      \u001b[33m<\u001b[39m\u001b[33mPage\u001b[39m \u001b[33m/\u001b[39m\u001b[33m>\u001b[39m\u001b[0m\n\u001b[0m \u001b[90m 29 | \u001b[39m    \u001b[33m<\u001b[39m\u001b[33m/\u001b[39m\u001b[33mStaticRouter\u001b[39m\u001b[33m>\u001b[39m\u001b[0m\n\u001b[0m \u001b[90m 30 | \u001b[39m  )\u001b[33m;\u001b[39m\u001b[0m\n    at Object._raise (C:\\Development\\MERN\\Issue-Tracker\\ui\\node_modules\\@babel\\parser\\lib\\index.js:764:17)\n    at Object.raiseWithData (C:\\Development\\MERN\\Issue-Tracker\\ui\\node_modules\\@babel\\parser\\lib\\index.js:757:17)\n    at Object.raise (C:\\Development\\MERN\\Issue-Tracker\\ui\\node_modules\\@babel\\parser\\lib\\index.js:751:17)\n    at Object.unexpected (C:\\Development\\MERN\\Issue-Tracker\\ui\\node_modules\\@babel\\parser\\lib\\index.js:8927:16)\n    at Object.parseExprAtom (C:\\Development\\MERN\\Issue-Tracker\\ui\\node_modules\\@babel\\parser\\lib\\index.js:10227:20)\n    at Object.parseExprAtom (C:\\Development\\MERN\\Issue-Tracker\\ui\\node_modules\\@babel\\parser\\lib\\index.js:4686:20)\n    at Object.parseExprSubscripts (C:\\Development\\MERN\\Issue-Tracker\\ui\\node_modules\\@babel\\parser\\lib\\index.js:9789:23)\n    at Object.parseUpdate (C:\\Development\\MERN\\Issue-Tracker\\ui\\node_modules\\@babel\\parser\\lib\\index.js:9769:21)\n    at Object.parseMaybeUnary (C:\\Development\\MERN\\Issue-Tracker\\ui\\node_modules\\@babel\\parser\\lib\\index.js:9758:17)\n    at Object.parseExprOps (C:\\Development\\MERN\\Issue-Tracker\\ui\\node_modules\\@babel\\parser\\lib\\index.js:9628:23)\n    at Object.parseMaybeConditional (C:\\Development\\MERN\\Issue-Tracker\\ui\\node_modules\\@babel\\parser\\lib\\index.js:9602:23)\n    at Object.parseMaybeAssign (C:\\Development\\MERN\\Issue-Tracker\\ui\\node_modules\\@babel\\parser\\lib\\index.js:9565:21)\n    at Object.parseExpression (C:\\Development\\MERN\\Issue-Tracker\\ui\\node_modules\\@babel\\parser\\lib\\index.js:9517:23)\n    at Object.jsxParseExpressionContainer (C:\\Development\\MERN\\Issue-Tracker\\ui\\node_modules\\@babel\\parser\\lib\\index.js:4537:30)\n    at Object.jsxParseAttributeValue (C:\\Development\\MERN\\Issue-Tracker\\ui\\node_modules\\@babel\\parser\\lib\\index.js:4504:21)\n    at Object.jsxParseAttribute (C:\\Development\\MERN\\Issue-Tracker\\ui\\node_modules\\@babel\\parser\\lib\\index.js:4555:44)\n    at Object.jsxParseOpeningElementAfterName (C:\\Development\\MERN\\Issue-Tracker\\ui\\node_modules\\@babel\\parser\\lib\\index.js:4575:28)\n    at Object.jsxParseOpeningElementAt (C:\\Development\\MERN\\Issue-Tracker\\ui\\node_modules\\@babel\\parser\\lib\\index.js:4568:17)\n    at Object.jsxParseElementAt (C:\\Development\\MERN\\Issue-Tracker\\ui\\node_modules\\@babel\\parser\\lib\\index.js:4600:33)\n    at Object.jsxParseElement (C:\\Development\\MERN\\Issue-Tracker\\ui\\node_modules\\@babel\\parser\\lib\\index.js:4674:17)\n    at Object.parseExprAtom (C:\\Development\\MERN\\Issue-Tracker\\ui\\node_modules\\@babel\\parser\\lib\\index.js:4681:19)\n    at Object.parseExprSubscripts (C:\\Development\\MERN\\Issue-Tracker\\ui\\node_modules\\@babel\\parser\\lib\\index.js:9789:23)\n    at Object.parseUpdate (C:\\Development\\MERN\\Issue-Tracker\\ui\\node_modules\\@babel\\parser\\lib\\index.js:9769:21)\n    at Object.parseMaybeUnary (C:\\Development\\MERN\\Issue-Tracker\\ui\\node_modules\\@babel\\parser\\lib\\index.js:9758:17)\n    at Object.parseExprOps (C:\\Development\\MERN\\Issue-Tracker\\ui\\node_modules\\@babel\\parser\\lib\\index.js:9628:23)\n    at Object.parseMaybeConditional (C:\\Development\\MERN\\Issue-Tracker\\ui\\node_modules\\@babel\\parser\\lib\\index.js:9602:23)\n    at Object.parseMaybeAssign (C:\\Development\\MERN\\Issue-Tracker\\ui\\node_modules\\@babel\\parser\\lib\\index.js:9565:21)\n    at Object.parseParenAndDistinguishExpression (C:\\Development\\MERN\\Issue-Tracker\\ui\\node_modules\\@babel\\parser\\lib\\index.js:10418:28)\n    at Object.parseExprAtom (C:\\Development\\MERN\\Issue-Tracker\\ui\\node_modules\\@babel\\parser\\lib\\index.js:10122:21)\n    at Object.parseExprAtom (C:\\Development\\MERN\\Issue-Tracker\\ui\\node_modules\\@babel\\parser\\lib\\index.js:4686:20)");
+
+/***/ }),
+
+/***/ "./server/template.js":
+false,
+
+/***/ "./src/About.jsx":
+false,
+
+/***/ "./src/Contents.jsx":
+false,
+
+/***/ "./src/IssueAddNavItem.jsx":
+false,
+
+/***/ "./src/IssueDetail.jsx":
+false,
+
+/***/ "./src/IssueEdit.jsx":
+false,
+
+/***/ "./src/IssueFilter.jsx":
+false,
+
+/***/ "./src/IssueList.jsx":
+false,
+
+/***/ "./src/IssueReport.jsx":
+false,
+
+/***/ "./src/IssueTable.jsx":
+false,
+
+/***/ "./src/NotFound.jsx":
+false,
+
+/***/ "./src/Page.jsx":
+false,
+
+/***/ "./src/Toasts.jsx":
+false,
+
+/***/ "./src/graphQLFetch.js":
+false,
+
+/***/ "./src/routes.js":
+false,
+
+/***/ "./src/store.js":
+false,
+
+/***/ "isomorphic-fetch":
+false,
+
+/***/ "prop-types":
+false,
+
+/***/ "react":
+false,
+
+/***/ "react-bootstrap":
+false,
+
+/***/ "react-bootstrap/Alert":
+false,
+
+/***/ "react-bootstrap/Button":
+false,
+
+/***/ "react-bootstrap/ButtonGroup":
+false,
+
+/***/ "react-bootstrap/ButtonToolbar":
+false,
+
+/***/ "react-bootstrap/Card":
+false,
+
+/***/ "react-bootstrap/Col":
+false,
+
+/***/ "react-bootstrap/Container":
+false,
+
+/***/ "react-bootstrap/Form":
+false,
+
+/***/ "react-bootstrap/FormControl":
+false,
+
+/***/ "react-bootstrap/Modal":
+false,
+
+/***/ "react-bootstrap/Nav":
+false,
+
+/***/ "react-bootstrap/NavDropdown":
+false,
+
+/***/ "react-bootstrap/Navbar":
+false,
+
+/***/ "react-bootstrap/Row":
+false,
+
+/***/ "react-bootstrap/Table":
+false,
+
+/***/ "react-bootstrap/Toast":
+false,
+
+/***/ "react-bootstrap/ToastHeader":
+false,
+
+/***/ "react-dom/server":
+false,
+
+/***/ "react-icons/bs":
+false,
+
+/***/ "react-router-bootstrap":
+false,
+
+/***/ "react-router-dom":
+false,
+
+/***/ "serialize-javascript":
+false,
+
+/***/ "url-search-params":
+false
+
+};
+//# sourceMappingURL=server.31b6a7c5a6f417d8485f.hot-update.js.map
---
+++ ui/dist/server.e80088d191fe8583cbe1.hot-update.js
@@ -0,0 +1,145 @@
+exports.id = "server";
+exports.modules = {
+
+/***/ "./server/render.jsx":
+/*!***************************!*\
+  !*** ./server/render.jsx ***!
+  \***************************/
+/*! exports provided: default */
+/***/ (function(module, exports) {
+
+throw new Error("Module build failed (from ./node_modules/babel-loader/lib/index.js):\nSyntaxError: C:\\Development\\MERN\\Issue-Tracker\\ui\\server\\render.jsx: Unexpected keyword 'const' (26:2)\n\n\u001b[0m \u001b[90m 24 | \u001b[39m  store\u001b[33m.\u001b[39minitialData \u001b[33m=\u001b[39m initialData\u001b[33m;\u001b[39m\u001b[0m\n\u001b[0m \u001b[90m 25 | \u001b[39m  \u001b[36mconst\u001b[39m \u001b[0m\n\u001b[0m\u001b[31m\u001b[1m>\u001b[22m\u001b[39m\u001b[90m 26 | \u001b[39m  \u001b[36mconst\u001b[39m element \u001b[33m=\u001b[39m (\u001b[0m\n\u001b[0m \u001b[90m    | \u001b[39m  \u001b[31m\u001b[1m^\u001b[22m\u001b[39m\u001b[0m\n\u001b[0m \u001b[90m 27 | \u001b[39m    \u001b[33m<\u001b[39m\u001b[33mStaticRouter\u001b[39m location\u001b[33m=\u001b[39m{req\u001b[33m.\u001b[39murl} context\u001b[33m=\u001b[39m{{}}\u001b[33m>\u001b[39m\u001b[0m\n\u001b[0m \u001b[90m 28 | \u001b[39m      \u001b[33m<\u001b[39m\u001b[33mPage\u001b[39m \u001b[33m/\u001b[39m\u001b[33m>\u001b[39m\u001b[0m\n\u001b[0m \u001b[90m 29 | \u001b[39m    \u001b[33m<\u001b[39m\u001b[33m/\u001b[39m\u001b[33mStaticRouter\u001b[39m\u001b[33m>\u001b[39m\u001b[0m\n    at Object._raise (C:\\Development\\MERN\\Issue-Tracker\\ui\\node_modules\\@babel\\parser\\lib\\index.js:764:17)\n    at Object.raiseWithData (C:\\Development\\MERN\\Issue-Tracker\\ui\\node_modules\\@babel\\parser\\lib\\index.js:757:17)\n    at Object.raise (C:\\Development\\MERN\\Issue-Tracker\\ui\\node_modules\\@babel\\parser\\lib\\index.js:751:17)\n    at Object.checkReservedWord (C:\\Development\\MERN\\Issue-Tracker\\ui\\node_modules\\@babel\\parser\\lib\\index.js:11056:12)\n    at Object.parseIdentifierName (C:\\Development\\MERN\\Issue-Tracker\\ui\\node_modules\\@babel\\parser\\lib\\index.js:11026:12)\n    at Object.parseIdentifier (C:\\Development\\MERN\\Issue-Tracker\\ui\\node_modules\\@babel\\parser\\lib\\index.js:10997:23)\n    at Object.parseBindingAtom (C:\\Development\\MERN\\Issue-Tracker\\ui\\node_modules\\@babel\\parser\\lib\\index.js:9291:17)\n    at Object.parseVarId (C:\\Development\\MERN\\Issue-Tracker\\ui\\node_modules\\@babel\\parser\\lib\\index.js:12018:20)\n    at Object.parseVar (C:\\Development\\MERN\\Issue-Tracker\\ui\\node_modules\\@babel\\parser\\lib\\index.js:11994:12)\n    at Object.parseVarStatement (C:\\Development\\MERN\\Issue-Tracker\\ui\\node_modules\\@babel\\parser\\lib\\index.js:11806:10)\n    at Object.parseStatementContent (C:\\Development\\MERN\\Issue-Tracker\\ui\\node_modules\\@babel\\parser\\lib\\index.js:11398:21)\n    at Object.parseStatement (C:\\Development\\MERN\\Issue-Tracker\\ui\\node_modules\\@babel\\parser\\lib\\index.js:11331:17)\n    at Object.parseBlockOrModuleBlockBody (C:\\Development\\MERN\\Issue-Tracker\\ui\\node_modules\\@babel\\parser\\lib\\index.js:11913:25)\n    at Object.parseBlockBody (C:\\Development\\MERN\\Issue-Tracker\\ui\\node_modules\\@babel\\parser\\lib\\index.js:11899:10)\n    at Object.parseBlock (C:\\Development\\MERN\\Issue-Tracker\\ui\\node_modules\\@babel\\parser\\lib\\index.js:11883:10)\n    at Object.parseFunctionBody (C:\\Development\\MERN\\Issue-Tracker\\ui\\node_modules\\@babel\\parser\\lib\\index.js:10901:24)\n    at Object.parseFunctionBodyAndFinish (C:\\Development\\MERN\\Issue-Tracker\\ui\\node_modules\\@babel\\parser\\lib\\index.js:10884:10)\n    at C:\\Development\\MERN\\Issue-Tracker\\ui\\node_modules\\@babel\\parser\\lib\\index.js:12053:12\n    at Object.withTopicForbiddingContext (C:\\Development\\MERN\\Issue-Tracker\\ui\\node_modules\\@babel\\parser\\lib\\index.js:11206:14)\n    at Object.parseFunction (C:\\Development\\MERN\\Issue-Tracker\\ui\\node_modules\\@babel\\parser\\lib\\index.js:12052:10)\n    at Object.parseFunctionStatement (C:\\Development\\MERN\\Issue-Tracker\\ui\\node_modules\\@babel\\parser\\lib\\index.js:11679:17)\n    at Object.parseStatementContent (C:\\Development\\MERN\\Issue-Tracker\\ui\\node_modules\\@babel\\parser\\lib\\index.js:11456:25)\n    at Object.parseStatement (C:\\Development\\MERN\\Issue-Tracker\\ui\\node_modules\\@babel\\parser\\lib\\index.js:11331:17)\n    at Object.parseBlockOrModuleBlockBody (C:\\Development\\MERN\\Issue-Tracker\\ui\\node_modules\\@babel\\parser\\lib\\index.js:11913:25)\n    at Object.parseBlockBody (C:\\Development\\MERN\\Issue-Tracker\\ui\\node_modules\\@babel\\parser\\lib\\index.js:11899:10)\n    at Object.parseTopLevel (C:\\Development\\MERN\\Issue-Tracker\\ui\\node_modules\\@babel\\parser\\lib\\index.js:11262:10)\n    at Object.parse (C:\\Development\\MERN\\Issue-Tracker\\ui\\node_modules\\@babel\\parser\\lib\\index.js:12967:10)\n    at parse (C:\\Development\\MERN\\Issue-Tracker\\ui\\node_modules\\@babel\\parser\\lib\\index.js:13020:38)\n    at parser (C:\\Development\\MERN\\Issue-Tracker\\ui\\node_modules\\@babel\\core\\lib\\parser\\index.js:54:34)\n    at parser.next (<anonymous>)");
+
+/***/ }),
+
+/***/ "./server/template.js":
+false,
+
+/***/ "./src/About.jsx":
+false,
+
+/***/ "./src/Contents.jsx":
+false,
+
+/***/ "./src/IssueAddNavItem.jsx":
+false,
+
+/***/ "./src/IssueDetail.jsx":
+false,
+
+/***/ "./src/IssueEdit.jsx":
+false,
+
+/***/ "./src/IssueFilter.jsx":
+false,
+
+/***/ "./src/IssueList.jsx":
+false,
+
+/***/ "./src/IssueReport.jsx":
+false,
+
+/***/ "./src/IssueTable.jsx":
+false,
+
+/***/ "./src/NotFound.jsx":
+false,
+
+/***/ "./src/Page.jsx":
+false,
+
+/***/ "./src/Toasts.jsx":
+false,
+
+/***/ "./src/graphQLFetch.js":
+false,
+
+/***/ "./src/routes.js":
+false,
+
+/***/ "./src/store.js":
+false,
+
+/***/ "isomorphic-fetch":
+false,
+
+/***/ "prop-types":
+false,
+
+/***/ "react":
+false,
+
+/***/ "react-bootstrap":
+false,
+
+/***/ "react-bootstrap/Alert":
+false,
+
+/***/ "react-bootstrap/Button":
+false,
+
+/***/ "react-bootstrap/ButtonGroup":
+false,
+
+/***/ "react-bootstrap/ButtonToolbar":
+false,
+
+/***/ "react-bootstrap/Card":
+false,
+
+/***/ "react-bootstrap/Col":
+false,
+
+/***/ "react-bootstrap/Container":
+false,
+
+/***/ "react-bootstrap/Form":
+false,
+
+/***/ "react-bootstrap/FormControl":
+false,
+
+/***/ "react-bootstrap/Modal":
+false,
+
+/***/ "react-bootstrap/Nav":
+false,
+
+/***/ "react-bootstrap/NavDropdown":
+false,
+
+/***/ "react-bootstrap/Navbar":
+false,
+
+/***/ "react-bootstrap/Row":
+false,
+
+/***/ "react-bootstrap/Table":
+false,
+
+/***/ "react-bootstrap/Toast":
+false,
+
+/***/ "react-bootstrap/ToastHeader":
+false,
+
+/***/ "react-dom/server":
+false,
+
+/***/ "react-icons/bs":
+false,
+
+/***/ "react-router-bootstrap":
+false,
+
+/***/ "react-router-dom":
+false,
+
+/***/ "serialize-javascript":
+false,
+
+/***/ "url-search-params":
+false
+
+};
+//# sourceMappingURL=server.e80088d191fe8583cbe1.hot-update.js.map
