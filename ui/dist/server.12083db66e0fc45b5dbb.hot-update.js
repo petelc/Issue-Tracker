e9c326de45389026e104b1c72233c97320b4a0dd
Pete Carroll <petelc1966@hotmail.com>
2020-09-03 05:47:31 -0400

    updated nodemon and mongodb

---
+++ ui/dist/server.12083db66e0fc45b5dbb.hot-update.js
@@ -0,0 +1,211 @@
+exports.id = "server";
+exports.modules = {
+
+/***/ "./server/render.jsx":
+false,
+
+/***/ "./server/template.js":
+false,
+
+/***/ "./server/uiserver.js":
+/*!****************************!*\
+  !*** ./server/uiserver.js ***!
+  \****************************/
+/*! no exports provided */
+/***/ (function(module, exports) {
+
+throw new Error("Module build failed (from ./node_modules/babel-loader/lib/index.js):\nError: ENOENT: no such file or directory, open 'C:\\Development\\MERN\\Issue-Tracker\\ui\\server\\uiserver.js'");
+
+/***/ }),
+
+/***/ "./src/About.jsx":
+false,
+
+/***/ "./src/Auth/LoginButton.jsx":
+false,
+
+/***/ "./src/Auth/LogoutButton.jsx":
+false,
+
+/***/ "./src/Contents.jsx":
+false,
+
+/***/ "./src/IssueAddNavItem.jsx":
+false,
+
+/***/ "./src/IssueDetail.jsx":
+false,
+
+/***/ "./src/IssueEdit.jsx":
+false,
+
+/***/ "./src/IssueFilter.jsx":
+false,
+
+/***/ "./src/IssueList.jsx":
+false,
+
+/***/ "./src/IssueReport.jsx":
+false,
+
+/***/ "./src/IssueTable.jsx":
+false,
+
+/***/ "./src/NotFound.jsx":
+false,
+
+/***/ "./src/Page.jsx":
+false,
+
+/***/ "./src/Search.jsx":
+false,
+
+/***/ "./src/Toasts.jsx":
+false,
+
+/***/ "./src/graphQLFetch.js":
+false,
+
+/***/ "./src/routes.js":
+false,
+
+/***/ "./src/store.js":
+false,
+
+/***/ "./src/withToasts.jsx":
+false,
+
+/***/ "./webpack.config.js":
+false,
+
+/***/ "@auth0/auth0-react":
+false,
+
+/***/ "dotenv":
+false,
+
+/***/ "express":
+false,
+
+/***/ "http-proxy-middleware":
+false,
+
+/***/ "isomorphic-fetch":
+false,
+
+/***/ "path":
+false,
+
+/***/ "prop-types":
+false,
+
+/***/ "react":
+false,
+
+/***/ "react-bootstrap":
+false,
+
+/***/ "react-bootstrap/Alert":
+false,
+
+/***/ "react-bootstrap/Badge":
+false,
+
+/***/ "react-bootstrap/Button":
+false,
+
+/***/ "react-bootstrap/ButtonGroup":
+false,
+
+/***/ "react-bootstrap/ButtonToolbar":
+false,
+
+/***/ "react-bootstrap/Card":
+false,
+
+/***/ "react-bootstrap/Col":
+false,
+
+/***/ "react-bootstrap/Collapse":
+false,
+
+/***/ "react-bootstrap/Container":
+false,
+
+/***/ "react-bootstrap/Form":
+false,
+
+/***/ "react-bootstrap/FormControl":
+false,
+
+/***/ "react-bootstrap/Modal":
+false,
+
+/***/ "react-bootstrap/Nav":
+false,
+
+/***/ "react-bootstrap/NavDropdown":
+false,
+
+/***/ "react-bootstrap/NavItem":
+false,
+
+/***/ "react-bootstrap/Navbar":
+false,
+
+/***/ "react-bootstrap/Pagination":
+false,
+
+/***/ "react-bootstrap/Row":
+false,
+
+/***/ "react-bootstrap/Table":
+false,
+
+/***/ "react-bootstrap/Toast":
+false,
+
+/***/ "react-bootstrap/ToastHeader":
+false,
+
+/***/ "react-dom/server":
+false,
+
+/***/ "react-icons/bs":
+false,
+
+/***/ "react-icons/go":
+false,
+
+/***/ "react-router-bootstrap":
+false,
+
+/***/ "react-router-dom":
+false,
+
+/***/ "react-select/lib/Async":
+false,
+
+/***/ "serialize-javascript":
+false,
+
+/***/ "source-map-support":
+false,
+
+/***/ "url-search-params":
+false,
+
+/***/ "webpack":
+false,
+
+/***/ "webpack-dev-middleware":
+false,
+
+/***/ "webpack-hot-middleware":
+false,
+
+/***/ "webpack-node-externals":
+false
+
+};
+//# sourceMappingURL=server.12083db66e0fc45b5dbb.hot-update.js.map
---
+++ ui/src/Config/AuthConfig.js
@@ -0,0 +1,9 @@
+const APP_DOMAIN = 'dev-qjd5lg0y.us.auth0.com';
+const APP_CLIENTID = 'bx0Qdjwo4gfJgg225vEBnfiQe85kxP0T';
+const APP_CALLBACKURL = 'http://localhost:8000';
+
+const authKeys = [
+  APP_DOMAIN, APP_CLIENTID, APP_CALLBACKURL,
+];
+
+export default authKeys;
